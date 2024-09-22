a934bca82cc3386f3467b0a540c934a0fe2c2979
S1NGS1NG80 <160927065+S1NGS1NG80@users.noreply.github.com>
2024-09-22 18:28:07 +0300

    adjusted css on EventsPage.jsx

--- client/src/pages/EventsPage.jsx
+++ client/src/pages/EventsPage.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import EventCreationForm from '../components/EventForm';
+import EventForm from '../components/EventForm';
 import EventList from '../components/EventList';
 
 const EventsPage = () => {
   return (
-    <div className="container mx-auto">
-      <EventCreationForm />
+    <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
+      <EventForm />
       <EventList />
     </div>
   );
