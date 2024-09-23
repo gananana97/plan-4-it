d672aa4a24f037b5829d112b505a6c9b37cc351f
S1NGS1NG80 <160927065+S1NGS1NG80@users.noreply.github.com>
2024-09-24 01:44:24 +0300

    minor changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import EventsPage from './pages/EventsPage';
-import RSVPPage from './pages/RSVPPage';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import EventsPage from "./pages/EventsPage";
+import RSVPPage from "./pages/RSVPPage";
 
 function App() {
   return (
     <Router>
+      <nav className="p-4 bg-gray-800 text-white">
+        <ul className="flex space-x-4">
+          <li>
+            <Link to="/" className="hover:underline">
+              Events
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
       <Routes>
         <Route path="/" element={<EventsPage />} />
         <Route path="/rsvp/:eventId/:guestId" element={<RSVPPage />} />
+        <Route
+          path="*"
+          element={<h2 className="text-center">404 - Page Not Found</h2>}
+        />
       </Routes>
     </Router>
   );
