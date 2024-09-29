2f7647cb13d419500e97d91a7c47cab94ea4edca
l.maines <lmmaines28@gmail.com>
2024-09-28 23:48:11 -0400

    added loginUser to api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -44,3 +44,15 @@ export const getAllTech = () => {
     },
   });
 };
+
+
+export const loginUser = async (credentials) => {
+  return fetch('/api/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(credentials),
+  });
+};
+
