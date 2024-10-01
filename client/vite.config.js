b3e83eba2aec124e1279c32f369712cad938750a
l.maines <lmmaines28@gmail.com>
2024-09-30 21:40:52 -0400

    reconfigure

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
   plugins: [react()],
+  base: './', // Ensures relative paths in production
   server: {
     port: 3000,
     open: true,
@@ -13,5 +11,8 @@ export default defineConfig({
         secure: false,
       },
     },
+  },
+  build: {
+    outDir: 'dist', // Optional: Vite's default is 'dist', so you can omit this if 'dist' is your desired folder
   },
 });
